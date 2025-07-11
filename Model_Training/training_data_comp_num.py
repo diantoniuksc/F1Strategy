80570aca8047a3b04464fb5eca3800670f82da03
diantoniuksc <162093949+diantoniuksc@users.noreply.github.com>
2025-07-11 16:23:07 +0200

    Prepared data for creation of the basic model

--- Model_Training/training_data_comp_num.py
+++ Model_Training/training_data_comp_num.py
@@ -13,15 +13,16 @@ def get_training_data_comp_num(weight_coef_compound: int):
     training_data = training_data[training_data['is_valid'] == 1]
 
     # One-hot encode categorical columns
-    categorical_cols = ['driver_id', 'team_id', 'race_name']
+    #categorical_cols = ['driver_id', 'team_id', 'race_name']
+    categorical_cols = ['race_name']
     training_data_encoded = pd.get_dummies(training_data, columns=categorical_cols)
 
     # Give more weight to tyre compound columns
-    compound_cols = [(col for col in training_data_encoded.columns if col=='compound')]
+    '''compound_cols = [(col for col in training_data_encoded.columns if col=='compound')]
     for col in compound_cols:
-        training_data_encoded[col] *= weight_coef_compound
+        training_data_encoded[col] *= weight_coef_compound'''
 
-    x = training_data_encoded.drop(columns=['tyre_life', 'is_valid'])
+    x = training_data_encoded.drop(columns=['tyre_life', 'is_valid', 'stint_start_lap', 'year', 'driver_id', 'team_id'])
     y = training_data_encoded[['tyre_life']]
 
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
